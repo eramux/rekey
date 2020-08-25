a3ecd60705b59707ef5e8fe7dbf548074d2fef36
Daniel Schneider <daniel.schneider@eramux.com>
2020-08-25 20:13:54 +0200

    fix: remove dist folder

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { RekeyObject } from "./rekey";
-export declare function renameKey(object: RekeyObject, key: string, value: string): void;
-export declare function deleteKey(object: RekeyObject, key: string): void;
-//# sourceMappingURL=index.d.ts.map
